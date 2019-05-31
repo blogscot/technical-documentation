97145a9e96a55d83533df0b30e853048806a3caa
Iain Diamond <iain.diamond@gmail.com>
2019-05-31 11:21:03 +0100

    Handle menu toggling for window resizing

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 window.onload = function() {
   const labelToggle = document.querySelector('#label-toggle')
   const navBar = document.querySelector('nav#navbar')
+  const listItems = [...document.querySelectorAll('nav ul>a')]
 
-  const fccWrapper = document.querySelector('#fcc_test_suite_wrapper')
+  const maxSmallDeviceWidth = 500
+  const width = window.innerWidth
+
   let menuOpen = true
-
-  // hide FCC wrapper
-  fccWrapper.classList.add('hideWrapper')
+  let menuItemsCanHideSideBar = false
 
   labelToggle.addEventListener('click', function() {
     if (menuOpen) {
@@ -18,4 +19,29 @@ window.onload = function() {
     }
     menuOpen = !menuOpen
   })
+
+  if (width < maxSmallDeviceWidth) {
+    menuItemsCanHideSideBar = true
+    listItems.forEach(item => item.addEventListener('click', enable))
+  }
+
+  window.onresize = function() {
+    const width = window.innerWidth
+    if (menuItemsCanHideSideBar && width > maxSmallDeviceWidth) {
+      // Force the navbar open
+      labelToggle.classList.remove('toggleMenu')
+      navBar.classList.remove('hideNavbar')
+
+      menuItemsCanHideSideBar = false
+      listItems.forEach(item => item.removeEventListener('click', enable))
+    } else if (!menuItemsCanHideSideBar && width < maxSmallDeviceWidth) {
+      menuItemsCanHideSideBar = true
+      listItems.forEach(item => item.addEventListener('click', enable))
+    }
+  }
+  function enable() {
+    labelToggle.classList.add('toggleMenu')
+    navBar.classList.add('hideNavbar')
+    menuOpen = false
+  }
 }
